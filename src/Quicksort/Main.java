7b5d1b89e7ab80fbea32417dcc01809edb779b94
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-04-06 16:32:57 +1300

    Derp

--- src/Quicksort/Main.java
+++ src/Quicksort/Main.java
@@ -10,20 +10,24 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import javax.transaction.xa.Xid;
 
 public class Main {
-	public static int PROCESSORS = 4;
+	private static int PROCESSORS = 4;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		ExecutorService threads = Executors.newFixedThreadPool(PROCESSORS);
+
 		// readfile("Resources/nums1.txt");
 
 		List<Integer> toSort = new ArrayList<Integer>();
 		Random rand = new Random();
-		for (int i = 0; i < 100000; i++) {
+		for (int i = 0; i < 1000; i++) {
 			toSort.add(rand.nextInt(1000));
 		}
 
@@ -32,23 +36,84 @@ public class Main {
 		int index = toSort.size() / PROCESSORS;
 		// threads.execute(new Sorter(l));
 		for (int i = 1; i < PROCESSORS + 1; i++) {
-			List<Integer> l = toSort.subList(index * (i - 1), index * i);
+			List<Integer> l = new ArrayList<Integer>(toSort.subList(index
+					* (i - 1), index * i));
 			processorLists.add(l);
+			Sorter s = new Sorter(l);
+			sorterList.add(s);
+		}
+
+		for (Sorter s : sorterList) {
+			threads.execute(s);
+		}
+
+		threads.shutdown();
+		try {
+			threads.awaitTermination(10000, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		threads = Executors.newFixedThreadPool(1);
+
+		List<Integer> samples = new ArrayList<Integer>();
+
+		for (Sorter s : sorterList) {
+			System.out.println(s);
+			samples.addAll(s.getSamples(PROCESSORS));
+		}
+
+		Sorter seqQS = new Sorter(samples);
+		threads.execute(seqQS);
+		threads.shutdown();
+		try {
+			threads.awaitTermination(10000, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		threads = Executors.newFixedThreadPool(PROCESSORS);
+
+		List<Integer> points = seqQS.getSamples(PROCESSORS);
+		points.remove(0);
+
+		// This is my favourite line
+		List<List<List<Integer>>> sectionList = new ArrayList<List<List<Integer>>>();
+
+		for (Sorter s : sorterList) {
+			sectionList.add(s.getSections(points));
+		}
+
+		// merge the section sections at the same indices.
+		sorterList.clear();
+
+		for (int i = 0; i < PROCESSORS; i++) {
+			List<Integer> l = new ArrayList<Integer>();
+
+			for (int j = 0; j < sectionList.size(); i++) {
+				l.addAll(sectionList.get(j).get(i));
+			}
+
 			Sorter s = new Sorter(l);
 			threads.execute(s);
 			sorterList.add(s);
 		}
 
 		threads.shutdown();
+		try {
+			threads.awaitTermination(10000, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		threads = Executors.newFixedThreadPool(PROCESSORS);
+
 		for (Sorter s : sorterList) {
 			printList(s.getSorted());
-			System.out
-					.println("=====================================================");
+			// System.out.println("\n=====================================================");
 		}
-
 	}
 
-	public static List<Integer> readfile(String filename) {
+	private static List<Integer> readfile(String filename) {
 		List<Integer> list = new ArrayList<Integer>();
 		Path path = Paths.get(filename);
 		try {
@@ -63,13 +128,13 @@ public class Main {
 		return list;
 	}
 
-	public static void printList(List<Integer> list) {
+	private static void printList(List<Integer> list) {
 		boolean first = true;
 		for (Integer i : list) {
 			if (!first) {
 				System.out.print(",");
 			}
-			System.out.print(i);
+			System.out.print(String.format("%3d", i));
 			first = false;
 		}
 	}

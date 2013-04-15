28d6dd390b8557112ad5210daec8c876c1183e92
Andrew Hughson <andrew@hughson.me>
2013-04-15 19:53:14 +1200

    Use a barrier to synchronize threads rather than destroying and creating a new threadpool

--- src/Quicksort/RunnableQuickSort.java
+++ src/Quicksort/RunnableQuickSort.java
@@ -2,16 +2,27 @@ package Quicksort;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 public class RunnableQuickSort extends SequentialQuickSort implements Runnable {
+	CyclicBarrier barrier;
 
-	public RunnableQuickSort(List<Integer> unsorted) {
+	public RunnableQuickSort(List<Integer> unsorted, CyclicBarrier barrier) {
 		super(unsorted);
+		this.barrier = barrier;
 	}
 
 	@Override
 	public void run() {
 		super.sortList();
+		
+		try {
+			System.out.println("Num waiting on thread " + Thread.currentThread().getName() + ": " + barrier.getNumberWaiting());
+			barrier.await();
+		} catch (InterruptedException | BrokenBarrierException e) {
+			return;
+		}
 	}
 
 	public List<List<Integer>> getSections(List<Integer> points) {

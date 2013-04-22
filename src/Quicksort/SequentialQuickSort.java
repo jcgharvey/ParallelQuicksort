bd5635953a5c8d412d34ebceb22a4ece8b29e278
ahug048 <ahug048@ae9d7370-e26b-4fef-b4cd-2f8f7d514ad8>
2013-04-22 03:06:19 +0000

    Initial Commit
    Sequential Quicksort, and beginnings of PSRS
    
    git-svn-id: https://svn.ece.auckland.ac.nz/svn/se751-2013-g13/trunk@2 ae9d7370-e26b-4fef-b4cd-2f8f7d514ad8

---
+++ src/Quicksort/SequentialQuickSort.java
@@ -0,0 +1,29 @@
+package Quicksort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SequentialQuickSort implements Sort {
+	@Override
+	public List<Integer> SortList(List<Integer> list) {
+		if (list.size() <= 1) {
+			return list;
+		}
+		int pivot = list.remove(0);
+		List<Integer> less = new ArrayList<Integer>();
+		List<Integer> greater = new ArrayList<Integer>();
+		for (int i : list) {
+			if (i <= pivot) {
+				less.add(i);
+			} else {
+				greater.add(i);
+			}
+		}
+		// concatenate the lists
+		List<Integer> result = new ArrayList<Integer>();
+		result.addAll(SortList(less));
+		result.add(pivot);
+		result.addAll(SortList(greater));
+		return result;
+	}
+}
---
+++ src/Quicksort/Sort.java
@@ -0,0 +1,7 @@
+package Quicksort;
+
+import java.util.List;
+
+public interface Sort {
+	public List<Integer> SortList(List<Integer> list);
+}

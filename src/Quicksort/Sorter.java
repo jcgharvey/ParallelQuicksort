955d971a4e3729698f7105a5e1af2c6ab238582f
ahug048 <ahug048@ae9d7370-e26b-4fef-b4cd-2f8f7d514ad8>
2013-04-22 03:06:44 +0000

    Derp
    git-svn-id: https://svn.ece.auckland.ac.nz/svn/se751-2013-g13/trunk@5 ae9d7370-e26b-4fef-b4cd-2f8f7d514ad8

--- src/Quicksort/Sorter.java
+++ src/Quicksort/Sorter.java
@@ -7,12 +7,19 @@ public class Sorter implements Runnable {
 
 	private List<Integer> unsortedList;
 	private List<Integer> sortedList;
+	private List<Integer> samples;
+	private List<List<Integer>> sections;
 	private boolean sorted;
 
 	public Sorter(List<Integer> unsorted) {
 		this.unsortedList = unsorted;
 		this.sorted = false;
 	}
+
+	// public Sorter(List<Integer> unsorted) {
+	// this.unsortedList = unsorted;
+	// this.sorted = false;
+	// }
 
 	@Override
 	public void run() {
@@ -42,11 +49,56 @@ public class Sorter implements Runnable {
 		return result;
 	}
 
+	public List<Integer> getSamples(int processors) {
+		samples = new ArrayList<Integer>();
+		for (int i = 0; i < processors; i++) {
+			System.out.println("SL:" + sortedList.size());
+			samples.add(sortedList.get(i * sortedList.size() / (processors)));
+		}
+		System.out.println("SA:" + samples.size());
+		return samples;
+	}
+
 	public boolean isSorted() {
 		return sorted;
 	}
-	
+
 	public List<Integer> getSorted() {
 		return sortedList;
+	}
+
+	private void findSections(List<Integer> points) {
+		sections = new ArrayList<List<Integer>>();
+
+		int currentPointIndex = 0;
+		int from = 0;
+		int point = points.get(currentPointIndex);
+
+		for (int i = 0; i < sortedList.size(); i++) {
+			if (sortedList.get(i) > point) {
+				sections.add(sortedList.subList(from, i));
+				from = i;
+				currentPointIndex += 1;
+				if (currentPointIndex >= points.size()) {
+					sections.add(sortedList.subList(from, sortedList.size() - 1));
+					break;
+				}
+				
+				point = points.get(currentPointIndex);
+			}
+		}
+
+		// this code is dicks
+//		for (int i = sections.size(); i <= points.size() + 1; i++) {
+//			sections.add(new ArrayList<Integer>());
+//		}
+
+		System.out.println("SS:" + sections.size());
+	}
+
+	public List<List<Integer>> getSections(List<Integer> points) {
+		if (sections == null)
+			findSections(points);
+		return sections;
 	}
 }

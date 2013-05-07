539bd307ab125b01e78e270dc1f6798f86a7b470
ahug048 <ahug048@ae9d7370-e26b-4fef-b4cd-2f8f7d514ad8>
2013-05-07 08:33:08 +0000

    Refactor QuickSorter
    git-svn-id: https://svn.ece.auckland.ac.nz/svn/se751-2013-g13/trunk@38 ae9d7370-e26b-4fef-b4cd-2f8f7d514ad8

--- src/se751/team13/quicksort/Main.java
+++ src/se751/team13/quicksort/Main.java
@@ -1,13 +1,11 @@
 package se751.team13.quicksort;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 
 public class Main {
 
-	private static final int NUM_NUMBERS = 100000;
+	private static final int NUM_NUMBERS = 10000;
 	private static final int MAX_NUMBER = 1000;
 
 	public static void main(String[] args) throws InterruptedException,
@@ -19,13 +17,14 @@ public class Main {
 		long end;
 
 		start = System.currentTimeMillis();
-		Sorter sqs = new SequentialQuicksort();
+		Sorter<Integer> sqs = new SequentialQuicksort<Integer>();
 		sqs.sort(unsortedNumbers);
 		end = System.currentTimeMillis();
+
 		System.out.println("time seq (ms): " + (end - start));
 
 		start = System.currentTimeMillis();
-		Sorter pqs = new ParallelQuicksort();
+		Sorter<Integer> pqs = new ParallelQuicksort<Integer>();
 		pqs.sort(unsortedNumbers);
 		end = System.currentTimeMillis();
 

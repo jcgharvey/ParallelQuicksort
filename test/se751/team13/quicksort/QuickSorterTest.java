789c5d1419fd7c579369cd2768b543136649740a
ahug048 <ahug048@ae9d7370-e26b-4fef-b4cd-2f8f7d514ad8>
2013-05-07 08:33:01 +0000

    Factor out utility methods
    git-svn-id: https://svn.ece.auckland.ac.nz/svn/se751-2013-g13/trunk@35 ae9d7370-e26b-4fef-b4cd-2f8f7d514ad8

--- test/se751/team13/quicksort/QuickSorterTest.java
+++ test/se751/team13/quicksort/QuickSorterTest.java
@@ -2,11 +2,9 @@ package se751.team13.quicksort;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.BrokenBarrierException;
 
 import org.junit.Test;
@@ -25,22 +23,22 @@ public class QuickSorterTest<T extends Comparable<? super T>> {
 		return Arrays
 				.asList(new Object[][] {
 						{ new ParallelQuicksort(),
-								generateRandomNumbers(100, 50) },
+								Util.generateRandomNumbers(100, 50) },
 						{ new ParallelQuicksort(),
-								generateRandomNumbers(1000, 500) },
+								Util.generateRandomNumbers(1000, 500) },
 						{ new ParallelQuicksort(),
-								generateRandomNumbers(10000, 50000) },
-						{ new ParallelQuicksort(), generateInOrderNumbers(100) },
-						{ new ParallelQuicksort(), generateInOrderNumbers(1000) },
+								Util.generateRandomNumbers(10000, 50000) },
+						{ new ParallelQuicksort(), Util.generateInOrderNumbers(100) },
+						{ new ParallelQuicksort(), Util.generateInOrderNumbers(1000) },
 						{ new ParallelQuicksort(),
-								generateInOrderNumbers(10000) },
-						{ new QuickSorter(), generateRandomNumbers(100, 50) },
-						{ new QuickSorter(), generateRandomNumbers(1000, 500) },
+								Util.generateInOrderNumbers(10000) },
+						{ new QuickSorter(), Util.generateRandomNumbers(100, 50) },
+						{ new QuickSorter(), Util.generateRandomNumbers(1000, 500) },
 						{ new QuickSorter(),
-								generateRandomNumbers(10000, 50000) },
-						{ new QuickSorter(), generateInOrderNumbers(100) },
-						{ new QuickSorter(), generateInOrderNumbers(1000) },
-						{ new QuickSorter(), generateInOrderNumbers(10000) } });
+								Util.generateRandomNumbers(10000, 50000) },
+						{ new QuickSorter(), Util.generateInOrderNumbers(100) },
+						{ new QuickSorter(), Util.generateInOrderNumbers(1000) },
+						{ new QuickSorter(), Util.generateInOrderNumbers(10000) } });
 	}
 
 	public QuickSorterTest(Sorter sorter, List<Integer> unsorted) {
@@ -48,47 +46,11 @@ public class QuickSorterTest<T extends Comparable<? super T>> {
 		this.unsorted = unsorted;
 	}
 
-	private static List<Integer> generateRandomNumbers(int amount, int max) {
-		Random rand = new Random(System.currentTimeMillis());
-		List<Integer> nums = new ArrayList<Integer>();
-
-		for (int i = 0; i < amount; i++) {
-			nums.add(rand.nextInt(max));
-		}
-		return nums;
-	}
-
-	private static List<Integer> generateInOrderNumbers(int amount) {
-		List<Integer> nums = new ArrayList<Integer>();
-
-		for (int i = 0; i < amount; i++) {
-			nums.add(i);
-		}
-		return nums;
-	}
-
-	private boolean inOrder(List<Integer> list) {
-
-		Integer previous, current;
-		previous = list.get(0);
-
-		for (int i = 1; i < list.size(); i++) {
-			current = list.get(i);
-
-			if (previous.compareTo(current) > 0)
-				return false;
-
-			previous = current;
-		}
-
-		return true;
-	}
-
 	@Test
 	public void testSorted() throws InterruptedException,
 			BrokenBarrierException {
 		sorted = sorter.sort(unsorted);
-		assertTrue(inOrder(sorted));
+		assertTrue(Util.inOrder(sorted));
 	}
 
 	@Test

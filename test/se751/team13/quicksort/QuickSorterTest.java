14bc8314da4591ffd0550f131f1d0b664b9813ab
ahug048 <ahug048@ae9d7370-e26b-4fef-b4cd-2f8f7d514ad8>
2013-05-07 08:32:43 +0000

    Refactor tests
    git-svn-id: https://svn.ece.auckland.ac.nz/svn/se751-2013-g13/trunk@27 ae9d7370-e26b-4fef-b4cd-2f8f7d514ad8

--- test/se751/team13/quicksort/QuickSorterTest.java
+++ test/se751/team13/quicksort/QuickSorterTest.java
@@ -14,9 +14,9 @@ public class QuickSorterTest<T extends Comparable<? super T>> {
 	private final int LARGE = 10000;
 	private final int LOW_MAX = 1000;
 	private final int HIGH_MAX = 100000;
-			
-	Sorter pqs = new ParallelQuicksort();	
-	
+
+	Sorter pqs = new ParallelQuicksort();
+
 	private static List<Integer> generateRandomNumbers(int amount, int max) {
 		Random rand = new Random(System.currentTimeMillis());
 		List<Integer> nums = new ArrayList<Integer>();
@@ -26,7 +26,7 @@ public class QuickSorterTest<T extends Comparable<? super T>> {
 		}
 		return nums;
 	}
-	
+
 	private static List<Integer> generateInOrderNumbers(int amount) {
 		List<Integer> nums = new ArrayList<Integer>();
 
@@ -35,115 +35,93 @@ public class QuickSorterTest<T extends Comparable<? super T>> {
 		}
 		return nums;
 	}
-	
+
+	private boolean inOrder(List<Integer> list) {
+
+		Integer previous, current;
+		previous = list.get(0);
+
+		for (int i = 1; i < list.size(); i++) {
+			current = list.get(i);
+
+			if (previous.compareTo(current) > 0)
+				return false;
+
+			previous = current;
+		}
+
+		return true;
+	}
+
 	@Test
-	public void testSmallRandList(){
+	public void testSmallRandList() throws InterruptedException,
+			BrokenBarrierException {
 		List<Integer> unsorted = generateRandomNumbers(10, 100);
 		List<Integer> sorted = null;
-		try {
-			
-			sorted = pqs.sort(unsorted);
-		} catch (InterruptedException | BrokenBarrierException e) {
-			e.printStackTrace();
-		}
-		Integer previous = null;
-		for (Integer current : sorted) {
-			if (previous != null) {
-				assertTrue(previous.compareTo(current) < 0);
-			}
-		}	
-		assertTrue(sorted.size() == unsorted.size());		
+
+		sorted = pqs.sort(unsorted);
+
+		assertTrue(inOrder(sorted));
+		assertTrue(sorted.size() == unsorted.size());
 	}
-	
+
 	@Test
-	public void testSmallOrderList(){
+	public void testSmallOrderList() throws InterruptedException,
+			BrokenBarrierException {
 		List<Integer> unsorted = generateInOrderNumbers(10);
 		List<Integer> sorted = null;
-		try {
-			sorted = pqs.sort(unsorted);
-		} catch (InterruptedException | BrokenBarrierException e) {
-			e.printStackTrace();
-		}
-		Integer previous = null;
-		for (Integer current : sorted) {
-			if (previous != null) {
-				assertTrue(previous.compareTo(current) < 0);
-			}
-		}	
+
+		sorted = pqs.sort(unsorted);
+
+		assertTrue(inOrder(sorted));
 		assertTrue(sorted.size() == unsorted.size());
 	}
-	
+
 	@Test
-	public void testRandOrderShortInteger() {
-		List<Integer> unsorted = generateRandomNumbers(SHORT,
-				LOW_MAX);
+	public void testRandOrderShortInteger() throws InterruptedException,
+			BrokenBarrierException {
+		List<Integer> unsorted = generateRandomNumbers(SHORT, LOW_MAX);
 		List<Integer> sorted = null;
-		try {
-			sorted = pqs.sort(unsorted);
-		} catch (InterruptedException | BrokenBarrierException e) {
-			e.printStackTrace();
-		}
-		Integer previous = null;
-		for (Integer current : sorted) {
-			if (previous != null) {
-				assertTrue(previous.compareTo(current) < 0);
-			}
-		}
+
+		sorted = pqs.sort(unsorted);
+
+		assertTrue(inOrder(sorted));
 		assertTrue(sorted.size() == unsorted.size());
 	}
-	
+
 	@Test
-	public void testInOrderShortInteger() {
+	public void testInOrderShortInteger() throws InterruptedException,
+			BrokenBarrierException {
 		List<Integer> unsorted = generateInOrderNumbers(SHORT);
 		List<Integer> sorted = null;
-		try {
-			sorted = pqs.sort(unsorted);
-		} catch (InterruptedException | BrokenBarrierException e) {
-			e.printStackTrace();
-		}
-		Integer previous = null;
-		for (Integer current : sorted) {
-			if (previous != null) {
-				assertTrue(previous.compareTo(current) < 0);
-			}
-		}	
+
+		sorted = pqs.sort(unsorted);
+
+		assertTrue(inOrder(sorted));
 		assertTrue(sorted.size() == unsorted.size());
 	}
-	
+
 	@Test
-	public void testRandOrderLargeInteger() {
-		List<Integer> unsorted = generateRandomNumbers(LARGE,
-				HIGH_MAX);
+	public void testRandOrderLargeInteger() throws InterruptedException,
+			BrokenBarrierException {
+		List<Integer> unsorted = generateRandomNumbers(LARGE, HIGH_MAX);
 		List<Integer> sorted = null;
-		try {
-			sorted = pqs.sort(unsorted);
-		} catch (InterruptedException | BrokenBarrierException e) {
-			e.printStackTrace();
-		}
-		Integer previous = null;
-		for (Integer current : sorted) {
-			if (previous != null) {
-				assertTrue(previous.compareTo(current) < 0);
-			}
-		}	
+
+		sorted = pqs.sort(unsorted);
+
+		assertTrue(inOrder(sorted));
 		assertTrue(sorted.size() == unsorted.size());
 	}
-		
+
 	@Test
-	public void testInOrderLargeInteger() {
+	public void testInOrderLargeInteger() throws InterruptedException,
+			BrokenBarrierException {
 		List<Integer> unsorted = generateInOrderNumbers(LARGE);
 		List<Integer> sorted = null;
-		try {
-			sorted = pqs.sort(unsorted);
-		} catch (InterruptedException | BrokenBarrierException e) {
-			e.printStackTrace();
-		}
-		Integer previous = null;
-		for (Integer current : sorted) {
-			if (previous != null) {
-				assertTrue(previous.compareTo(current) < 0);
-			}
-		}	
+
+		sorted = pqs.sort(unsorted);
+
+		assertTrue(inOrder(sorted));
 		assertTrue(sorted.size() == unsorted.size());
 	}
 }

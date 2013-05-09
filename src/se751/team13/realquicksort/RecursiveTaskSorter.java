ec273eeb222280b849c86648c44a9c2046b15c14
Jourdan Harvey <jourdan.harvey@gmail.com>
2013-05-09 21:53:21 +1200

    Realised that there a few changes required in next commit. Commiting a breaking commit to store current state. Can easily be fixed to working from here. not sure exactly what last commit was, im on the bus :/

---
+++ src/se751/team13/realquicksort/RecursiveTaskSorter.java
@@ -0,0 +1,29 @@
+package se751.team13.realquicksort;
+
+import java.util.List;
+import java.util.concurrent.BrokenBarrierException;
+
+import se751.team13.quicksort.Sorter;
+
+public class RecursiveTaskSorter<T extends Comparable<? super T>> implements Sorter<T> {
+
+	
+	private int processors;
+
+	public RecursiveTaskSorter(){
+		processors = Runtime.getRuntime().availableProcessors();
+	}
+	
+	@Override
+	public List<T> sort(List<T> unsorted) throws InterruptedException,
+			BrokenBarrierException {
+		// TODO Auto-generated method stub
+		QuickSort qs = new QuickSort(processors);
+		qs.add_task(unsorted, 0, unsorted.size() -1);
+		qs.work_wait();
+		
+		
+		return unsorted;
+	}
+	
+}
